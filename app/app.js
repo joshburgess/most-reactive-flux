4674a942087e020deff4393cd1a789f9e9e9e88c
Denis Stoyanov <stoyanov.gr@gmail.com>
2016-03-02 08:48:30 +0200

    Has been added undo/redo

--- app/app.js
+++ app/app.js
@@ -1,34 +1,47 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
+import {Map, List} from 'immutable';
+
 import createStore from './src/createStore';
 import reducer from './src/reducer';
 import {streamable} from './src/utils';
-import {INCREMENT_COUNTER, INCREMENT_MANY_COUNTER, DECREMENT_COUNTER, DECREMENT_MANY_COUNTER, RESET_COUNTER} from './src/constants';
+import {INCREMENT_COUNTER, INCREMENT_MANY_COUNTER,
+    DECREMENT_COUNTER, DECREMENT_MANY_COUNTER, RESET_COUNTER, UNDO_COUNTER, REDO_COUNTER, REMOVE_HISTORY} from './src/constants';
 
 const store = createStore(reducer, new Promise(resolver => {
-    setTimeout(resolver.bind(resolver, 42), 500);
+    setTimeout(resolver.bind(resolver, Map({counter: 42, history: List(), future: List()})), 500);
 }));
 
 // We can use it like a decorator or usual approach like
 // Counter = streamable(store, x => x.map(_ => `Item => ${_}`))(Counter) || Counter;
 
-@streamable(store, x => x.map(_ => `Item => ${_}`))
+@streamable(store)
 class Counter extends Component {
     render() {
         const {dispatch, data} = this.props;
         return <div>
-            <button className="btn btn-danger" onClick={() => dispatch(DECREMENT_MANY_COUNTER, ...[1, 2])}>Click -1 and -2</button>
-            &nbsp;
-            <button className="btn btn-danger" onClick={() => dispatch(DECREMENT_COUNTER, 1)}>Click -1</button>
-            &nbsp;
-            <span>{!!data ? data : 0}</span>
-            &nbsp;
-            <button className="btn btn-info" onClick={() => dispatch(INCREMENT_COUNTER, 1)}>Click +1</button>
-            &nbsp;
-            <button className="btn btn-info" onClick={() => dispatch(INCREMENT_MANY_COUNTER, ...[1, 2])}>Click +1 and +2</button>
-            &nbsp;
-            <button className="btn btn-success" onClick={() => dispatch(RESET_COUNTER)}>Reset</button>
+            {data && <div>
+                <div>
+                    <button disabled={data.get('history').size < 1} className="btn btn-success" onClick={() => dispatch(UNDO_COUNTER)}>Undo</button>
+                    &nbsp;
+                    <button disabled={data.get('future').size < 1} className="btn btn-success" onClick={() => dispatch(REDO_COUNTER)}>Redo</button>
+                    &nbsp;
+                    <div className="btn btn-success" onClick={() => dispatch(REMOVE_HISTORY)}>Clear history</div>
+                </div>
+                <br />
+                <button className="btn btn-danger" onClick={() => dispatch(DECREMENT_MANY_COUNTER, ...[1, 2])}>Click -1 and -2</button>
+                &nbsp;
+                <button className="btn btn-danger" onClick={() => dispatch(DECREMENT_COUNTER, 1)}>Click -1</button>
+                &nbsp;
+                <span>{!!data ? data.get('counter') : 0}</span>
+                &nbsp;
+                <button className="btn btn-info" onClick={() => dispatch(INCREMENT_COUNTER, 1)}>Click +1</button>
+                &nbsp;
+                <button className="btn btn-info" onClick={() => dispatch(INCREMENT_MANY_COUNTER, ...[1, 2])}>Click +1 and +2</button>
+                &nbsp;
+                <button className="btn btn-success" onClick={() => dispatch(RESET_COUNTER)}>Reset</button>
+            </div>}
         </div>
     }
 }
--- app/src/reducer.js
+++ app/src/reducer.js
@@ -2,12 +2,66 @@
  import subtract from 'ramda/src/subtract';
  import always from 'ramda/src/always';
 
- import { INCREMENT_COUNTER, INCREMENT_MANY_COUNTER, DECREMENT_COUNTER, DECREMENT_MANY_COUNTER, RESET_COUNTER } from './constants';
+ import {List} from 'immutable';
 
+ import { INCREMENT_COUNTER, INCREMENT_MANY_COUNTER,
+     DECREMENT_COUNTER, DECREMENT_MANY_COUNTER, RESET_COUNTER, UNDO_COUNTER, REDO_COUNTER, REMOVE_HISTORY } from './constants';
+ 
  export default {
-     [DECREMENT_MANY_COUNTER]: (x, y, z) => x - y - z,
-     [DECREMENT_COUNTER]: subtract, // ~ (x, y) => x - y
-     [INCREMENT_COUNTER]: add, // ~ (x, y) => x + y
-     [INCREMENT_MANY_COUNTER]: (x, y, z) => x + y + z,
-     [RESET_COUNTER]: always(0) // ~ x => 0
+     [DECREMENT_MANY_COUNTER]: (x, y, z) => {
+         x = x.update('history', history => history.push(x.get('counter')));
+         x = x.update('counter', v => v - y - z);
+
+         return x;
+     },
+     [DECREMENT_COUNTER]: (x, y) => {
+         x = x.update('history', history => history.push(x.get('counter')));
+         x = x.update('counter', v => v - y);
+
+         return x;
+     },//subtract, // ~ (x, y) => x - y
+     [INCREMENT_COUNTER]: (x, y) => {
+         x = x.update('history', history => history.push(x.get('counter')));
+         x = x.update('counter', v => v + y);
+
+         return x;
+     },//add, // ~ (x, y) => x + y
+     [INCREMENT_MANY_COUNTER]: (x, y, z) => {
+         x = x.update('history', history => history.push(x.get('counter')));
+         x = x.update('counter', v => v + y + z);
+
+         return x;
+     },
+     [RESET_COUNTER]: x => {
+         x = x.update('history', history => history.push(x.get('counter')));
+         x = x.set('counter', 0);
+
+         return x;
+     },//always(0) // ~ x => 0,
+     [UNDO_COUNTER]: x => {
+         if(x.get('history').size < 1) return x;
+         else {
+             const temp = x.get('history').last();
+             x = x.update('history', history => history.pop());
+             x = x.update('future', future => future.push(x.get('counter')));
+
+             return x.set('counter', temp);
+         }
+     },
+     [REDO_COUNTER]: x => {
+         if(x.get('future').size < 1) return x;
+         else {
+             const temp = x.get('future').last();
+             x = x.update('history', history => history.push(x.get('counter')));
+             x = x.update('future', future => future.pop());
+
+             return x.set('counter', temp);
+         }
+     },
+     [REMOVE_HISTORY]: x => {
+         x = x.set('history', List());
+         x = x.set('future', List());
+         
+         return x;
+     }
  };
